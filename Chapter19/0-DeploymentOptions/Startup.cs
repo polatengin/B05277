5f17c4ef96e70c04144a54898eb6f11e7bcd01b7
Engin Polat <engin@enginpolat.com>
2017-12-02 00:35:39 +0300

    changed the name of folder and project

--- Chapter19/0-DeploymentOptions/Startup.cs
+++ Chapter19/0-DeploymentOptions/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace GettingStarted
+namespace DeploymentOptions
 {
     public class Startup
     {
