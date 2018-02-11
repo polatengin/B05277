fa8d3336b60386e07dfa5b3aa359344338f212ed
Engin Polat <engin@enginpolat.com>
2018-02-11 23:17:19 +0300

    Chapter 1-2-3-4-5 is done :)

---
+++ Chapter05/4-ImplementingDIWithDryIoc/Bootstrap.cs
@@ -0,0 +1,7 @@
+public static class Bootstrap
+{
+    public static void RegisterServices(IRegistrator registrator)
+    {
+        registrator.Register<IProductService, ProductService>(Reuse.Singleton);
+    }
+}
---
+++ Chapter05/4-ImplementingDIWithDryIoc/IocDiExtensions.cs
@@ -0,0 +1,11 @@
+public static class IocDiExtensions
+{
+    public static IServiceProvider ConfigureDI(this IServiceProvider services, Action<Registrator> configureServices)
+    {
+        var container = new Container().WithDependencyInjectionAdapter(services);
+
+        configureServices(container);
+
+        return container.Resolve<IServiceProvider>();
+    }
+}
